50c1aa0161a72409220e2d210d307674e0e77896
youngjerald239 <youngjerald239@gmail.com>
2022-09-04 22:57:00 -0500

    added controls to camera

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import './style.css'
 
 import * as THREE from 'three'
 
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+
 const scene = new THREE.Scene()
 
 
@@ -29,6 +31,12 @@ pointLight.position.set(5, 5, 5)
 const ambientLight = new THREE.AmbientLight(0xffffff)
 scene.add(pointLight, ambientLight)
 
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper  = new THREE.GridHelper(200, 50)
+scene.add(lightHelper, gridHelper)
+
+const controls = new OrbitControls(camera, renderer.domElement)
+
 
 function animate(){
   requestAnimationFrame(animate)
@@ -36,6 +44,8 @@ function animate(){
   torus.rotation.x += 0.01
   torus.rotation.y += 0.005
   torus.rotation.z += 0.01
+
+  controls.update()
 
   renderer.render(scene, camera)
 }
